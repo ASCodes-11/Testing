743326a5269cf2cfa83c13c8c579bdd80d5a1160
ASCodes-11 <88712358+ASCodes-11@users.noreply.github.com>
2021-08-17 18:45:43 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+
+  if (keyisDown(DOWN_ARROW))  {
+
+     background ('red');
+
+  } 
+
+  
+  background(30);
+
+}
+
+
+
+
